3fafedc594bca331870e8d053243a8b335dd9499
Gabriel Castor <gabrielcastor1991@gmail.com>
2025-10-27 22:34:38 +0000

    Add MCP server (WIP)

---
+++ main.py
@@ -0,0 +1,52 @@
+from mcp.server.fastmcp import Context, FastMCP
+from mcp.server.session import ServerSession
+from enum import Enum
+import asyncio
+import httpx
+
+USER_AGENT = "Mozilla/5.0"  # "compliance-app/1.0"
+
+mcp = FastMCP(name="Compliance App")
+
+
+class LawCode(str, Enum):
+    civil = "civil"
+    forest = "forest"
+
+
+# TODO: replace hard-coded URLs with dynamic navigation to find the compiled versions
+# TODO: replace this function with a an API which returns the page contents. This way, it can
+# fallback to a default page containing the last version, whenever the URLs change
+def get_law_code_url(code: LawCode) -> str:
+    if LawCode.forest:
+        return "https://www.planalto.gov.br/ccivil_03/_Ato2011-2014/2012/Lei/L12651compilado.htm"
+    elif LawCode.civil:
+        return "https://www.planalto.gov.br/ccivil_03/Leis/2002/L10406compilada.htm"
+
+
+async def get_html_content(url: str) -> str:
+    headers = {"User-Agent": USER_AGENT}
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers, timeout=(1, 30))
+        response.raise_for_status()
+        return response.text
+
+
+async def get_law_code_raw_content(code: LawCode):
+    url = get_law_code_url(code)
+    html_content = await get_html_content(url)
+    return html_content
+
+
+@mcp.tool()
+async def get_law_code(code: LawCode, ctx: Context[ServerSession, None]) -> dict:
+    return {"content": get_law_code_raw_content(code)}
+
+
+async def main():
+    content = await get_law_code_raw_content(code=LawCode.forest)
+    print(content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ prototypes/prompt_2.py
@@ -0,0 +1,14 @@
+parecer = ""
+codigos = []
+
+prompt = """
+Dado o parecer a seguir:\n
+{parecer}
+Analise o documento acima, tendo em vistas as leis: {codigos}
+O resultado deve ter o seguinte formato:
+
+"Compliance checks:
+1. <compliance check 1>\n
+2. <compliance check 2>\n
+..."
+"""
